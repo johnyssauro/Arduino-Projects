b03027154e065320d48cf70a3cfdeb4d855cace9
João <joaopedronoronhadutra@gmail.com>
2024-04-04 00:43:46 -0300

    Implementing the void loop and creating distanceObj function

--- trena_eletronica/trena_eletronica.ino
+++ trena_eletronica/trena_eletronica.ino
@@ -1,8 +1,14 @@
 #define pinTrg 3
 #define pinEch 4
 
-// Speed of sound in m/s
-const float speedSound = 340;
+// Distance traveled by sound in 1 microsecond
+const float soundMUPS = 0.000340;
+
+float distanceObj(float timeMicroseconds) {
+
+  return (soundMUPS*timeMicroseconds)/2;
+
+}
 
 void setup() {
   
@@ -14,7 +20,19 @@ void setup() {
 }
 
 void loop() {
-  
 
+  // Shooting an ultrassonic pulse
+  digitalWrite(pinTrg, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinTrg, LOW);
+
+  // Time between Trigger shooting and Echo recepting the pulse
+
+  float time = pulseIn(pinEch, HIGH);
+
+  float distance = distanceObj(time);
+
+  Serial.println(distance);
+  delay(1000);
 
 }
